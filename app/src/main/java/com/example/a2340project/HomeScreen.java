71e0c897d300a3d14464cdf7dbf63fbadaf419fe
St George, Gabriel Christian <gabestgeorge@gmail.com>
2024-02-19 11:31:48 -0500

    Added Java Classes for Implementation 4
    Added Java classes for menu screen and activity screens

---
+++ app/src/main/java/com/example/a2340project/HomeScreen.java
@@ -0,0 +1,9 @@
+package com.example.a2340project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * to code or not to code.  I am bored.
+ */
+public class HomeScreen extends AppCompatActivity {
+}
