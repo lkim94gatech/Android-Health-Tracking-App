de659e3382fb8977a0e673cae3b83fbe6c27921c
SantiTvr <santovar@outlook.com>
2024-02-21 20:20:06 -0500

    added xml binding to all classes and made base layout with menu bar xml

--- app/src/main/java/com/example/a2340project/IngredientScreen.java
+++ app/src/main/java/com/example/a2340project/IngredientScreen.java
@@ -1,10 +1,17 @@
 package com.example.a2340project;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Class for the placeholder page for listing ingredients
  */
 public class IngredientScreen extends AppCompatActivity {
-    // insert swag code here
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient);
+    }
 }
--- app/src/main/java/com/example/a2340project/ShoppingListScreen.java
+++ app/src/main/java/com/example/a2340project/ShoppingListScreen.java
@@ -1,9 +1,16 @@
 package com.example.a2340project;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * twenty one pilots is releasing new music on the 29th apparently so that is hype
  */
 public class ShoppingListScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
 }
