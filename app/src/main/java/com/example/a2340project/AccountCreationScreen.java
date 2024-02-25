10af48d9c22e77a7eb90d6e37f9af54cc085f559
coffeefoxo420 <coffeefoxo@gmail.com>
2024-02-25 12:44:22 -0500

    Account Creation & Sign In
    pulls username and text and sends it to database for account creation and sign in.

--- app/src/main/java/com/example/a2340project/AccountCreationScreen.java
+++ app/src/main/java/com/example/a2340project/AccountCreationScreen.java
@@ -2,17 +2,29 @@ package com.example.a2340project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.database.DatabaseReference.CompletionListener;
 
 public class AccountCreationScreen extends AppCompatActivity {
     private EditText newUsernameText;
     private EditText newPasswordText;
     private FirebaseAuth mAuth;
+    private String username;
+    private String password;
+    private static final String TAG = "AccountCreationScreenActivity";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_creation);
@@ -29,11 +41,27 @@ public class AccountCreationScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AccountCreationScreen.this, HomeScreen.class);
-
+                username = newUsernameText.getText().toString();
+                password = newPasswordText.getText().toString();
                 /*
                 more view model stuff with retrieving text from text boxes
                  */
-
+                mAuth.createUserWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(AccountCreationScreen.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUser:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUser:failure", task.getException());
+                                    Toast.makeText(AccountCreationScreen.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                 //add data to firewall, I'll do later.
             }
         });
@@ -43,6 +71,8 @@ public class AccountCreationScreen extends AppCompatActivity {
                 Intent intent = new Intent(AccountCreationScreen.this, LoginScreen.class);
                 startActivity(intent);
             }
+
+
         });
     }
 }
