c4a2f7683416d3e7cc3d101cd462e4581a20a70e
cshaw44 <colinsidneyshaw@gmail.com>
2024-03-12 23:04:31 -0400

    updated readme

---
+++ app/src/main/java/com/example/a2340project/UserData.java
@@ -0,0 +1,33 @@
+package com.example.a2340project;
+
+public class UserData {
+    private int heightFeet;
+    private int heightInches;
+    private int weight;
+    private String gender;
+    public int getHeightFeet() {
+        return heightFeet;
+    }
+    public int getHeightInches() {
+        return heightInches;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setHeightFeet(int heightFeet) {
+        this.heightFeet = heightFeet;
+    }
+    public void setHeightInches(int heightInches) {
+        this.heightInches = heightInches;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+}
