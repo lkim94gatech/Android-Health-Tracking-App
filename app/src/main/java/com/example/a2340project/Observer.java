511a23cac9744254958d442b2e22ec41abef46af
lkim94gatech <lkim94@gatech.edu>
2024-04-06 00:51:35 -0400

    Observer design strategy

---
+++ app/src/main/java/com/example/a2340project/Observer.java
@@ -0,0 +1,8 @@
+package com.example.a2340project;
+
+/**
+ * Any class that needs to be notified about changes will implement this interface
+ */
+public interface Observer {
+    void update();
+}
---
+++ app/src/main/java/com/example/a2340project/PantryIngredientsModel.java
@@ -0,0 +1,50 @@
+package com.example.a2340project;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Concrete Observable
+ * responsible for managing the data related to pantry ingredients and notifying observers of any changes
+ */
+public class PantryIngredientsModel implements Observable {
+
+    private List<Observer> observers = new ArrayList<>();
+    private List<Recipe> recipes = new ArrayList<>();
+
+    private Map<String, Ingredient> ingredients;
+
+    public PantryIngredientsModel() {
+        // Initialize ingredients and set up Firebase listeners
+    }
+
+    // Observable methods
+    @Override
+    public void registerObserver(Observer o) {
+        if (!observers.contains(o)) {
+            observers.add(o);
+        }
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+
+    public List<Recipe> getUpdatedRecipes() {
+        return recipes;
+    }
+
+    // Method to be called when data changes
+    public void ingredientUpdated() {
+        notifyObservers();
+    }
+}
